7eb3fc443d99c82c7b91306f88e3cae96ce55933
Mauricio Molina <tica.molina57@gmail.com>
2025-04-20 22:21:43 -0300

    Hafo la configuración de CORS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'dotenv/config'
 import path from 'node:path'
+import cors from 'cors'
 import mongoDConnection from './utils/connection.js'
 import routerProducts from './routers/products.router.js'
 import routerUsers from './routers/users.router.js'
@@ -10,14 +11,17 @@ import routerUpload from './routers/uploads.router.js'
 const app = express()
 const PORT = 8080
 const URI_DB = process.env.URI_LOCAL
+const URL_FRONT = process.env.URL_FRONTEND_CORS
 
 // ! CONFIGURACIONES
-
+const corsConfig = {
+  origin: URL_FRONT // URL completa de netlify
+}
 
 // ! MIDDLEWARES
 app.use(express.json())
 app.use(express.static(path.join('public')))
-
+app.use(cors(corsConfig))
 
 // ! RUTAS
 app.use('/api/v1/productos', routerProducts)
